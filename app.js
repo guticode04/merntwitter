d080fd738dd7a44968db366159335d69433ead7b
Gustavo Gutierrez <gusguti04@gmail.com>
2020-03-14 21:54:52 -0700

    Create routes and validation

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 app.get("/", (req, res) => res.send("Hello"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
